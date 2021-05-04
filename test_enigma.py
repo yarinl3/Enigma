ead9d6894ca11328b3a579c62bc016756059d4df
yarinl3 <74721108+yarinl3@users.noreply.github.com>
2021-05-04 09:53:39 +0300

    Update test_enigma.py

--- test_enigma.py
+++ test_enigma.py
@@ -4,3 +4,10 @@ import enigma
 def test_fix_offset():
     test = enigma.fix_offset('c', 4)
     assert test == 6
+
+
+def test_encrypt():
+    settings = ['par', [3, 1, 2], [('a', 'g'), ('l', 's'), ('d', 'm'), ('n', 'q'), ('w', 'u'), ('z', 'c')]]
+    assert encrypt(settings, '') == ''
+    assert encrypt(settings, '   ') == ''
+    assert encrypt(settings, '\n') == ''
