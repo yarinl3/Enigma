f7d6d15cb1a376f88008fc4b7626403b9078e998
yarinl3 <74721108+yarinl3@users.noreply.github.com>
2021-05-04 10:03:18 +0300

    Update test_enigma.py

--- test_enigma.py
+++ test_enigma.py
@@ -7,4 +7,4 @@ def test_fix_offset():
 
 def test_encrypt():
     settings = ['par', [3, 1, 2], [('a', 'g'), ('l', 's'), ('d', 'm'), ('n', 'q'), ('w', 'u'), ('z', 'c')]]
-    assert encrypt(settings, 'yarin levi') == 'ictbnujmg'
+    assert encrypt(settings, 'yarinlevi') == 'ictbnujmg'
